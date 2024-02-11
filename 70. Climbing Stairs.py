2d3829744b37f25a47717ca0b65b868a8af1ee16
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2024-02-11 09:02:35 +0600

    working on leetcode 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,17 +1,16 @@
 class Solution:
   def climbStairs(self, n):
     count = 0 
-    for _ in range(n): 
-      sum = 0 
-      for i in range(n):
-        # print(i)
-        if sum == n:
-          count += 1
-          break
-        else: 
-          sum += i
-          i += 1
-    return count
+    sum = 0 
+    i = 0 
+    while (i < n): 
+      if sum == n: 
+        count += 1 
+        break 
+      else: 
+        sum += i
+      i += 1 
+    return count  
   
 
 s = Solution()
